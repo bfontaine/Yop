698d0ac8834f7f86e4657b408b473037457ee37d
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-07 15:37:06 +0100

    Yop.basepath attribute added

--- lib/yop/home.rb
+++ lib/yop/home.rb
@@ -3,13 +3,19 @@
 require "fileutils"
 
 module Yop
+  # The base path used for all files
+  @basepath = "~/.yop"
+
   class << self
+    # let applications change the base path
+    attr_accessor :basepath
+
     # Return the local Yop directory location. If an argument is given, it
     # assumes it's a Yop subcomponent and returns its location.
     # @param subcomponent [String] the subcomponent to look for
     # @return [String] the path to the local Yop directory or its subcomponent
     def home(subcomponent = "")
-      File.expand_path("~/.yop/#{subcomponent}")
+      File.expand_path("#{@basepath}/#{subcomponent}")
     end
 
     # Initialize Yop's local directory
